b12f237bc646c2f4cc3e2e2016564a4163ff7b07
“Oleg <olegbosyuk5@gmail.com>
2023-02-15 22:20:25 +0200

    Updated register test

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  port: 8080,
+  viewportHeight: 1080,
+  viewportWidth: 1920,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/Register.cy.js
+++ cypress/e2e/Register.cy.js
@@ -1,3 +1,17 @@
+import { faker } from '@faker-js/faker';
+import * as user from '../fixtures/user.json'
+
+let pass;
+
+user.firstName = faker.name.firstName();
+user.lastName = faker.name.lastName();
+user.email = faker.internet.email();
+user.address = faker.address.streetAddress();
+user.city = faker.address.city();
+user.postCode = faker.address.zipCode('####');
+user.username = faker.internet.userName();
+user.password = faker.internet.password(15);
+
 describe('Test register', () => {
 
   const register = () => {
@@ -5,17 +19,20 @@ describe('Test register', () => {
           cy.get('#customer_menu_top').click();
 
           cy.get('.btn-orange .fa-check').click()
-          cy.get('#AccountFrm_firstname').type('Oleg');
-          cy.get('#AccountFrm_lastname').type('Bosiuk');
-          cy.get('#AccountFrm_email').type('olegbosiuk55@gmail.com');
-          cy.get('#AccountFrm_address_1').type('some address');
-          cy.get('#AccountFrm_city').type('Kyiv');
+          cy.get('#AccountFrm_firstname').type(user.firstName);
+          cy.get('#AccountFrm_lastname').type(user.lastName);
+          cy.get('#AccountFrm_email').type(user.email);
+          cy.get('#AccountFrm_address_1').type(user.address);
+          cy.get('#AccountFrm_city').type(user.city);
           cy.get('#AccountFrm_country_id').select('Ukraine')
-          cy.get('#AccountFrm_zone_id').select('Kyiv');
-          cy.get('#AccountFrm_postcode').type('12345');
-          cy.get('#AccountFrm_loginname').type('OlegBest');
-          cy.get('#AccountFrm_password').type('12345');
-          cy.get('#AccountFrm_confirm').type('12345');
+          cy.get('#AccountFrm_zone_id')
+          .select(2, {force:true})
+          .invoke('val')
+          .should('not.eq', 'FALSE');
+          cy.get('#AccountFrm_postcode').type(user.postCode);
+          cy.get('#AccountFrm_loginname').type(user.userName);
+          cy.get('#AccountFrm_password').type(user.password);
+          cy.get('#AccountFrm_confirm').type(user.password);
           cy.get('#AccountFrm_agree').click();
           cy.get('.col-md-2 .btn').click();
           cy.get('.mb40 .btn').click();
@@ -23,6 +40,27 @@ describe('Test register', () => {
 
   it('name test case register',()=>{
         register()
+    } )
+
+  });
+
+
+describe('Test login', () => {
+
+  const log = () => {
+          
+          cy.visit('https://automationteststore.com/index.php?rt=account/login')
+          cy.get('#customer_menu_top').click();
+
+          cy.get('#loginFrm_loginname').type(user.userName);
+          cy.get('#loginFrm_password').type(user.password);
+          cy.get('#loginFrm > fieldset > .btn').click();
+
+
+        }
+
+  it('name test case login',()=>{
+        log()
     } )
 
   });
